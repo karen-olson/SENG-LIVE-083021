f4b206a8ef1b6765e14be9b58bb8df0e76b69284
Aysan Isayo <aisayo@icloud.com>
2021-10-24 21:25:53 -0700

    removed donations controller crud

--- phase-3/09-sinatra-crud/donation_tracker_server/app/controllers/donations_controller.rb
+++ phase-3/09-sinatra-crud/donation_tracker_server/app/controllers/donations_controller.rb
@@ -5,28 +5,5 @@ class DonationsController < ApplicationController
         donations = Donation.all
         donations.to_json
     end
-
-    post "/donations" do 
-        donation = Donation.create(
-            amount: params[:amount],
-            date: params[:date],
-            organization_id: params[:organization_id],
-            completed: params[:completed]
-        )
-        donation.donor = Donor.find_or_create_by(name: "aysan")
-        donation.to_json except: [:organization_id, :donor_id ]
-    end
-
-    patch "/donations/:id" do 
-        donation = Donation.find(params[:id])
-        donation.update(amount: params[:amount], date: params[:date])
-        donation.to_json
-    end
-
-    delete "/donations/:id" do 
-        donation = Donation.find(params[:id])
-        donation.destroy
-    end
     
-
 end
