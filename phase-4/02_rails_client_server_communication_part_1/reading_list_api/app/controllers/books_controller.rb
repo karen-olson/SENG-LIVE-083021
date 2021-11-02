4c4f292dda872aaeafa14e4d9af17f1e3c823bc9
Dakota L Martinez <dakotaleemusic@gmail.com>
2021-11-02 13:43:36 -0700

    end of part 2 p4l2

--- phase-4/02_rails_client_server_communication_part_1/reading_list_api/app/controllers/books_controller.rb
+++ phase-4/02_rails_client_server_communication_part_1/reading_list_api/app/controllers/books_controller.rb
@@ -6,4 +6,19 @@ class BooksController < ApplicationController
   def show
     render json: Book.find(params[:id]), include: [:readers]
   end
+
+  def create
+    book = Book.create(book_params)
+    if book.valid?
+      render json: book, status: :created
+    else
+      render json: { errors: book.errors }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def book_params
+    params.permit(:title, :description, :author, :cover_image_url)
+  end
 end
--- phase-4/02_rails_client_server_communication_part_1/reading_list_api/config/routes.rb
+++ phase-4/02_rails_client_server_communication_part_1/reading_list_api/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  resources :books, only: [:index, :show]
+  resources :books, only: [:index, :show, :create]
+  resources :user_books, only: [:create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
