7a1ff6c66302d1350addc93ad8ab786f731740cf
Dakota L Martinez <dakotaleemusic@gmail.com>
2021-11-02 15:08:51 -0700

    add associations back to meetup clone models

--- phase-4/02_rails_client_server_communication_part_1/meetup_clone_api/app/models/group.rb
+++ phase-4/02_rails_client_server_communication_part_1/meetup_clone_api/app/models/group.rb
@@ -1,5 +1,6 @@
 class Group < ApplicationRecord
-
+  has_many :user_groups
+  has_many :members, through: :user_groups, source: :user
   
   validates :name, presence: true, uniqueness: true
 end
--- phase-4/02_rails_client_server_communication_part_1/meetup_clone_api/app/models/user.rb
+++ phase-4/02_rails_client_server_communication_part_1/meetup_clone_api/app/models/user.rb
@@ -1,4 +1,3 @@
-
 class User < ApplicationRecord
   has_many :user_groups
   has_many :groups, through: :user_groups
