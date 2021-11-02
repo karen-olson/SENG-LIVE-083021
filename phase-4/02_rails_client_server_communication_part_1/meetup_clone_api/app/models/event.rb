c6f1a018548be7f1ff35bf7e7609f3b0c4f49ca3
Dakota L Martinez <dakotaleemusic@gmail.com>
2021-11-02 12:41:52 -0700

    end of part 1

--- phase-4/02_rails_client_server_communication_part_1/meetup_clone_api/app/models/event.rb
+++ phase-4/02_rails_client_server_communication_part_1/meetup_clone_api/app/models/event.rb
@@ -1,4 +1,7 @@
 class Event < ApplicationRecord
   belongs_to :group
   belongs_to :user
+
+  validates :title, :description, :location, :start_time, :end_time, {presence: true}
+  validates :title, uniqueness: { scope: [:location, :start_time]}
 end
