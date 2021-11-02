9dbafa3bad88874b198a0357ff1a536fe0783297
Dakota L Martinez <dakotaleemusic@gmail.com>
2021-11-02 12:41:52 -0700

    end of part 1

--- phase-4/02_rails_client_server_communication_part_1/meetup_clone_api/db/seeds.rb
+++ phase-4/02_rails_client_server_communication_part_1/meetup_clone_api/db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user = User.create(username: 'Dakota', email: 'dakota@dakota.com', bio: 'i love ruby')
+user2 = User.create(username: 'Dex', email: 'dex@dex.com', bio: 'i love js')
+
+group = Group.create(name: 'SENG-083021', location: 'everywhere!')
+
+event = user.created_events.create(
+  group: group,
+  title: 'Rails Fundamentals',
+  description: 'migrations generators and fun!',
+  start_time: Time.new(2021, 11, 1, 11),
+  end_time: Time.new(2021, 11, 1, 13)
+)
+
+event.attendees = [user, user2]
