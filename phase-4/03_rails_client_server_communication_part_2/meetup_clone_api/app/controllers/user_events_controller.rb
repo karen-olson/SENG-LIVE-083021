df06a85509590858df6546380becdcdb2158380f
Dakota L Martinez <dakotaleemusic@gmail.com>
2021-11-03 14:47:55 -0700

    solution to meetup todos for p4l3

--- phase-4/03_rails_client_server_communication_part_2/meetup_clone_api/app/controllers/user_events_controller.rb
+++ phase-4/03_rails_client_server_communication_part_2/meetup_clone_api/app/controllers/user_events_controller.rb
@@ -8,9 +8,26 @@ class UserEventsController < ApplicationController
     end
   end
 
+  def update
+    user_event = UserEvent.find(params[:id])
+    if user_event.update(update_user_event_params)
+      render json: user_event, status: :ok
+    else
+      render json: user_event.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    UserEvent.find(params[:id]).destroy
+  end
+
   private
 
   def user_event_params
     params.permit(:event_id)
+  end
+
+  def update_user_event_params
+    params.permit(:attended)
   end
 end
