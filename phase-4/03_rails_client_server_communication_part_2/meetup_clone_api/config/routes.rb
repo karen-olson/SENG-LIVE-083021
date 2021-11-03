932f7d8920c438fe467345dd0f4c5edbe74dd837
Dakota L Martinez <dakotaleemusic@gmail.com>
2021-11-03 14:47:55 -0700

    solution to meetup todos for p4l3

--- phase-4/03_rails_client_server_communication_part_2/meetup_clone_api/config/routes.rb
+++ phase-4/03_rails_client_server_communication_part_2/meetup_clone_api/config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
 
-  resources :user_groups, only: [:create]
-  resources :events, only: [:create]
-  resources :user_events, only: [:create]
+  resources :user_groups, only: [:create, :destroy]
+  resources :events, only: [:create, :update, :destroy]
+  resources :user_events, only: [:create, :update, :destroy]
   resources :groups, only: [:index, :show, :create]
   # resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
