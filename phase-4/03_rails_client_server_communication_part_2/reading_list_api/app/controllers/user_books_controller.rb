42c78ea8cd081daf024994fde225340d893fc62a
Dakota L Martinez <dakotaleemusic@gmail.com>
2021-11-03 15:02:25 -0700

    add solution to reading_list todos p4l3

--- phase-4/03_rails_client_server_communication_part_2/reading_list_api/app/controllers/user_books_controller.rb
+++ phase-4/03_rails_client_server_communication_part_2/reading_list_api/app/controllers/user_books_controller.rb
@@ -9,9 +9,26 @@ class UserBooksController < ApplicationController
     end
   end
 
+  def update
+    user_book = UserBook.find(params[:id])
+    if user_book.update(update_user_book_params)
+      render json: user_book, status: :ok
+    else
+      render json: { errors: user_book.errors }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    UserBook.find(params[:id]).destroy
+  end
+
   private
 
   def user_book_params
     params.permit(:book_id)
+  end
+
+  def update_user_book_params
+    params.permit(:read)
   end
 end
